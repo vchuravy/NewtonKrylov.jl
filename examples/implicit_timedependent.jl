ffdd5e852e9da98aff3b1211046120f311a01195
Valentin Churavy <v.churavy@gmail.com>
2025-03-04 12:44:26 +0100

    literate

--- examples/implicit_timedependent.jl
+++ examples/implicit_timedependent.jl
@@ -1,24 +1,37 @@
+# # Implicit time-integration
+
+# ## Necessary packages
 using NewtonKrylov
 using CairoMakie
+
+# ## Implicit schemes 
+
+# ### Implicit Euler
 
 function G_Euler!(res, f, y, yₙ, t, Δt)
     res .= yₙ .+ Δt .* f(y, t) .- y
 end
 
+# ### Implicit Midpoint
+
 function G_Midpoint!(res, f, y, yₙ, t, Δt)
     res .= yₙ .+ Δt .* f((yₙ .+ y) ./ 2, t + Δt / 2) .- y
 end
+
+# ### Implicit Trapezoid
 
 function G_Trapezoid!(res, f, y, yₙ, t, Δt)
     res .= yₙ .+ (Δt/2) .* (f(yₙ, t)  .+ f(y, t+Δt)) .- y
 end
 
+# ## Spring equations
 
 function f(x, t, γ)
     [ x[2],     # dx/dt = v 
     -γ^2* x[1]] # dv/dt = -γ^2 * x
 end
 
+# ## Non-adaptive time stepping
 
 function implicit_spring(G! = G_Euler!)
     k = 2.    # spring constant
@@ -48,6 +61,8 @@ function implicit_spring(G! = G_Euler!)
     end
     hist, ts
 end
+
+# ## Plots
 
 hist_euler, ts_euler = implicit_spring(G_Euler!)
 v_euler = map(y -> y[1], hist_euler)
