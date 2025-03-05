f7a61234d74300c09cc5736c0fa48b44800c71e0
Valentin Churavy <v.churavy@gmail.com>
2025-03-05 16:31:19 +0100

    Change example

--- examples/trixi.jl
+++ examples/trixi.jl
@@ -1,51 +1,81 @@
-# using OrdinaryDiffEqSSPRK, OrdinaryDiffEqLowStorageRK
 using Trixi
 
+# Example based on https://github.com/trixi-framework/Trixi.jl/blob/main/examples/tree_1d_dgsem/elixir_advection_extended.jl
+
 ###############################################################################
-# Semidiscretization of the quasi 1d shallow water equations
-# See Chan et al.  https://doi.org/10.48550/arXiv.2307.12089 for details
+# semidiscretization of the linear advection diffusion equation
 
-equations = ShallowWaterEquationsQuasi1D(gravity_constant = 9.81)
+advection_velocity = 0.1
+equations = LinearScalarAdvectionEquation1D(advection_velocity)
+diffusivity() = 0.1
+equations_parabolic = LaplaceDiffusion1D(diffusivity(), equations)
 
-initial_condition = initial_condition_convergence_test
+# Create DG solver with polynomial degree = 3 and (local) Lax-Friedrichs/Rusanov flux as surface flux
+solver = DGSEM(polydeg = 3, surface_flux = flux_lax_friedrichs)
 
-volume_flux = (flux_chan_etal, flux_nonconservative_chan_etal)
-surface_flux = (FluxPlusDissipation(flux_chan_etal, DissipationLocalLaxFriedrichs()),
-                flux_nonconservative_chan_etal)
+coordinates_min = -convert(Float64, pi) # minimum coordinate
+coordinates_max = convert(Float64, pi) # maximum coordinate
 
-dg = DGMulti(polydeg = 4, element_type = Line(), approximation_type = SBP(),
-             surface_integral = SurfaceIntegralWeakForm(surface_flux),
-             volume_integral = VolumeIntegralFluxDifferencing(volume_flux))
+# Create a uniformly refined mesh with periodic boundaries
+mesh = TreeMesh(
+    coordinates_min, coordinates_max,
+    initial_refinement_level = 4,
+    n_cells_max = 30_000, # set maximum capacity of tree data structure
+    periodicity = true
+)
 
-cells_per_dimension = (8,)
-mesh = DGMultiMesh(dg, cells_per_dimension,
-                   coordinates_min = (0.0,), coordinates_max = (sqrt(2),),
-                   periodicity = true)
-semi = SemidiscretizationHyperbolic(mesh, equations, initial_condition, dg;
-                                    source_terms = source_terms_convergence_test)
+function x_trans_periodic(
+        x, domain_length = SVector(oftype(x[1], 2 * pi)),
+        center = SVector(oftype(x[1], 0))
+    )
+    x_normalized = x .- center
+    x_shifted = x_normalized .% domain_length
+    x_offset = (
+        (x_shifted .< -0.5f0 * domain_length) -
+            (x_shifted .> 0.5f0 * domain_length)
+    ) .*
+        domain_length
+    return center + x_shifted + x_offset
+end
+
+# Define initial condition
+function initial_condition_diffusive_convergence_test(
+        x, t,
+        equation::LinearScalarAdvectionEquation1D
+    )
+    # Store translated coordinate for easy use of exact solution
+    x_trans = x_trans_periodic(x - equation.advection_velocity * t)
+
+    nu = diffusivity()
+    c = 0
+    A = 1
+    omega = 1
+    scalar = c + A * sin(omega * sum(x_trans)) * exp(-nu * omega^2 * t)
+    return SVector(scalar)
+end
+initial_condition = initial_condition_diffusive_convergence_test
+
+# define periodic boundary conditions everywhere
+boundary_conditions = boundary_condition_periodic
+boundary_conditions_parabolic = boundary_condition_periodic
+
+# A semidiscretization collects data structures and functions for the spatial discretization
+semi = SemidiscretizationHyperbolicParabolic(
+    mesh, (equations, equations_parabolic),
+    initial_condition,
+    solver;
+    boundary_conditions = (
+        boundary_conditions,
+        boundary_conditions_parabolic,
+    )
+)
 
 ###############################################################################
 # ODE solvers, callbacks etc.
 
+# Create ODE problem with time span from 0.0 to 1.0
 tspan = (0.0, 1.0)
 ode = semidiscretize(semi, tspan)
-
-summary_callback = SummaryCallback()
-
-analysis_interval = 100
-analysis_callback = AnalysisCallback(semi, interval = analysis_interval, uEltype = real(dg))
-
-alive_callback = AliveCallback(analysis_interval = analysis_interval)
-
-callbacks = CallbackSet(summary_callback,
-                        analysis_callback,
-                        alive_callback)
-
-###############################################################################
-# run the simulation
-
-# sol = solve(ode, RDPK3SpFSAL49(); abstol = 1.0e-8, reltol = 1.0e-8,
-#             ode_default_options()..., callback = callbacks)
 
 ##
 # Verify the gradient will work
@@ -74,8 +104,10 @@ tmp4 = zero(u0)
 ytmp = zero(u0)
 tmp1 = zero(u0)
 
-Enzyme.autodiff(Enzyme.Reverse, Enzyme.Duplicated(_f, _tmp6),
+Enzyme.autodiff(
+    Enzyme.Reverse, Enzyme.Duplicated(_f, _tmp6),
     Enzyme.Const, Enzyme.Duplicated(tmp3, tmp4),
     Enzyme.Duplicated(ytmp, tmp1),
     Enzyme.Duplicated(p, tmp2),
-    Enzyme.Const(t))
+    Enzyme.Const(t)
+)
