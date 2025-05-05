d5505a4595a41fc85513478b1068dfa5a99bdbbc
Valentin Churavy <v.churavy@gmail.com>
2025-05-05 15:16:12 +0200

    fix literate notebook

--- examples/heat_2D.jl
+++ examples/heat_2D.jl
@@ -10,21 +10,46 @@ include("halovector.jl")
 
 # ## Diffusion 2D
 
+# ### Boundary
+
+function bc_periodic!(u)
+    N, M = size(u)
+    N = N - 2
+    M = M - 2
+
+    # Enforce boundary conditions
+    # (wrap around)
+    u[0, :] .= u[N, :]
+    u[N + 1, :] .= u[1, :]
+    u[:, 0] .= u[:, N]
+    u[:, N + 1] .= u[:, 1]
+    return nothing
+end
+
+function bc_zero!(u)
+    N, M = size(u)
+    N = N - 2
+    M = M - 2
+
+    u[0, :] .= 0
+    u[N + 1, :] .= 0
+    u[:, 0] .= 0
+    u[:, N + 1] .= 0
+    return nothing
+end
+
+
 function diffusion!(du::HaloVector, u::HaloVector, p, t)
     return diffusion!(du.data, u.data, p, t)
 end
 
-function diffusion!(du, u, (a, Δx, Δy), _)
+function diffusion!(du, u, (a, Δx, Δy, bc!), _)
     N, M = size(u)
     N = N - 2
     M = M - 2
 
     ## Enforce boundary conditions
-    ## (wrap around)
-    u[0, :] .= u[N, :]
-    u[N + 1, :] .= u[1, :]
-    u[:, 0] .= u[:, N]
-    u[:, N + 1] .= u[:, 1]
+    bc!(u)
 
     for i in 1:N
         for j in 1:M
@@ -47,86 +72,78 @@ N = M = 40
 ## TODO: This is a time-step from explicit
 Δt = Δx^2 * Δy^2 / (2.0 * a * (Δx^2 + Δy^2)) # Largest stable time step
 
-u₀ = HaloVector(OffsetArray(zeros(N + 2, M + 2), 0:(N + 1), 0:(M + 1)))
+let
+    N = M = 12
+    u₀ = HaloVector(OffsetArray(zeros(N + 2, M + 2), 0:(N + 1), 0:(M + 1)))
+    jacobian(G_Euler!, diffusion!, u₀, (a, Δx, Δy, bc_zero!), Δt, 0.0)
+end
 
-@edit -u₀
+xs = 0.0:Δx:1.0
+ys = 0.0:Δy:1.0
 
-J = jacobian(G_Euler!, diffusion!, u₀, (a, Δx, Δy), Δt, 0.0)
-J2 = jacobian(G_Midpoint!, diffusion!, u₀, (a, Δx, Δy), Δt, 0.0)
-J3 = jacobian(G_Trapezoid!, diffusion!, u₀, (a, Δx, Δy), Δt, 0.0)
+function f(x, y)
+    return sin(π * x) .* sin(π * y)
+end
 
-heatmap(0:(N + 1), 0:(N + 1), u₀.data.parent)
+u₀ = let
+    _u₀ = zeros(N + 2, M + 2)
+    _u₀ .= f.(xs, ys')
+    HaloVector(OffsetArray(_u₀, 0:(N + 1), 0:(M + 1)))
+end
 
-function create_video_implicit(filename, G!, f!, N, M, p, Δt, t_stop, frame_kwargs)
-    xs = 0:(N + 1)
-    ys = 0:(M + 1)
+function plot_state(t, u, xs, ys)
+    data = @lift $u.data.parent
 
-    u = HaloVector(OffsetArray(zeros(N + 2, M + 2), xs, ys))
-    u.data.parent[N ÷ 2 .+ (-1:1), M ÷ 2 .+ (-1:1)] .= 5
+    xs_line = @lift @view $data[:, M ÷ 2]
+    ys_line = @lift @view $data[N ÷ 2, :]
 
+    fig = Figure()
+
+    ax = Axis(fig[1, 1])
+    heatmap!(ax, xs, ys, data)
+
+    ax1 = Axis(fig[2, 1])
+    lines!(ax1, xs, xs_line)
+
+    ax2 = Axis(fig[2, 2])
+    lines!(ax2, ys, ys_line)
+
+    fig[0, :] = Label(fig, @lift("t = $($t)"))
+
+    return fig
+end
+
+fig = plot_state(Observable(0.0), Observable(u₀), xs, ys)
+
+function create_video_implicit(filename, G!, f!, xs, ys, u, p, Δt, t_stop, frame_kwargs)
     ts = 0.0:Δt:t_stop
-    fig, ax, hm = heatmap(xs, ys, u.data.parent)
+    _u = Observable(u)
+    _t = Observable(first(ts))
+
+    fig = plot_state(_t, _u, xs, ys)
     return record(fig, filename; frame_kwargs...) do io
-        function callback(_)
-            Base.notify(hm.args[3])
+        function callback(__u)
+            _u[] = u
+            _t[] += Δt
             # autolimits!(ax) # update limits
             recordframe!(io)
             return yield()
         end
-        solve(G!, f!, u, p, Δt, ts; callback, verbose = 2)
+        solve(G!, f!, u, p, Δt, ts; callback, verbose = 1)
     end
 end
 
 create_video_implicit(
     joinpath(@__DIR__, "implicit_euler.mp4"),
-    G_Euler!, diffusion!, N, M, (a, Δx, Δy), Δt, 4 * Δt, (; framerate = 1)
+    G_Euler!, diffusion!, xs, ys, copy(u₀), (a, Δx, Δy, bc_zero!), Δt, 10.0, (; framerate = 30)
 )
 
+create_video_implicit(
+    joinpath(@__DIR__, "implicit_midpoint.mp4"),
+    G_Midpoint!, diffusion!, xs, ys, copy(u₀), (a, Δx, Δy, bc_zero!), Δt, 10.0, (; framerate = 30)
+)
 
-function explicit_diffusion(u₀, a, Δx, Δy, t_stop; callback = (u, t) -> nothing)
-    N, M = size(u₀)
-    Δt = Δx^2 * Δy^2 / (2.0 * a * (Δx^2 + Δy^2)) # Largest stable time step
-
-    u = u₀
-    du = zero(u)
-
-    u[N ÷ 2 .+ (-1:1), M ÷ 2 .+ (-1:1)] .= 5
-
-    t = 0.0
-    callback(u, t)
-
-    while t < t_stop
-        t += Δt
-        diffusion!(du, u, (a, Δx, Δy), t)
-        u .+= Δt .* du
-        callback(u, t)
-    end
-    return u
-end
-
-
-explicit_diffusion(zeros(N, M), 0.01, 0.01, 0.01, 0.2)
-implicit_diffusion(G_Midpoint!, zeros(N, M), 0.01, 0.01, 0.01, 0.02)
-
-function create_video(filename, method, N, M, method_args, frame_kwargs)
-    xs = 1:N
-    ys = 1:M
-
-    u = zeros(N, M)
-
-    fig, ax, hm = heatmap(xs, ys, u)
-    return record(fig, filename; frame_kwargs...) do io
-        function callback(_u, t)
-            Base.notify(hm.args[3])
-            ## autolimits!(ax) # update limits
-            recordframe!(io)
-            return yield()
-        end
-        method(u, method_args...; callback)
-    end
-end
-
-create_video(joinpath(@__DIR__, "explicit.mp4"), explicit_diffusion, N, M, (0.01, 0.01, 0.01, 0.2), (; framerate = 10))
-## create_video(joinpath(@__DIR__, "implicit_euler.mp4"), (args...; kwargs...)->implicit_diffusion(G_Euler!, args...; kwargs...), 40, 40, (0.01, 0.01, 0.01, 0.2), (;framerate=10))
-## create_video(joinpath(@__DIR__, "implicit_midpoint.mp4"), (args...; kwargs...)->implicit_diffusion(G_Midpoint!, args...; kwargs...), 40, 40, (0.01, 0.01, 0.01, 0.2), (;framerate=10))
-## create_video(joinpath(@__DIR__, "implicit_trapezoid.mp4"), (args...; kwargs...)->implicit_diffusion(G_Trapezoid!, args...; kwargs...), 40, 40, (0.01, 0.01, 0.01, 0.2), (;framerate=10))
+create_video_implicit(
+    joinpath(@__DIR__, "implicit_trapezoid.mp4"),
+    G_Trapezoid!, diffusion!, xs, ys, copy(u₀), (a, Δx, Δy, bc_zero!), Δt, 10.0, (; framerate = 30)
+)
